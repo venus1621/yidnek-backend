f4b367bf05085953af409414e8b803e0fc299633
Venus <venushailemeskel59@gmail.com>
2025-12-20 10:53:43 +0100

    December 13

--- models/Assessment.js
+++ models/Assessment.js
@@ -1,9 +1,12 @@
 
 import mongoose from "mongoose";
-const assessmentSchema = new mongoose.Schema({
-  classId:{ type: mongoose.Schema.Types.ObjectId, ref:"Class" },
-  teacherId:{ type: mongoose.Schema.Types.ObjectId, ref:"Teacher" },
-  title:String,
-  maxScore:Number
-},{timestamps:true});
+const assessmentSchema = new mongoose.Schema(
+  {
+    classId: { type: mongoose.Schema.Types.ObjectId, ref: "Class" },
+    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
+    course: String,
+    maxScore: Number,
+  },
+  { timestamps: true }
+);
 export default mongoose.model("Assessment", assessmentSchema);
