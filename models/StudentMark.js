7a0df767714a9c8bcca0df41e5e0affdee5118f7
Venus <venushailemeskel59@gmail.com>
2025-12-21 10:53:14 +0300

    December 13

--- models/StudentMark.js
+++ models/StudentMark.js
@@ -1,8 +1,12 @@
 
 import mongoose from "mongoose";
-const studentMarkSchema = new mongoose.Schema({
-  assessmentId:{ type: mongoose.Schema.Types.ObjectId, ref:"Assessment" },
-  studentId:{ type: mongoose.Schema.Types.ObjectId, ref:"Student" },
-  score:Number
-},{timestamps:true});
+const studentMarkSchema = new mongoose.Schema(
+  {
+    assessmentId: { type: mongoose.Schema.Types.ObjectId, ref: "Assessment" },
+    title: String,
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
+    score: Number,
+  },
+  { timestamps: true }
+);
 export default mongoose.model("StudentMark", studentMarkSchema);
